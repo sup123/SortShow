3152346a3061a142d7f59e9202feb4c614c48395
garbageman <damien.j.nagle@gmail.com>
2015-08-23 19:47:45 -0400

    Added more functions for sorting

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,11 +1,10 @@
-var sortStep = function(list, timer) {
-  /* Check if the list is in order */
-  if (isInOrder(list)) {
+var stepThrough = function(list) {
+  /* Set up the timer to do walkthrough */
+  var i = list.length - 1;
+  var j = 1;
+  var k = 0;
 
-  } else {
-    /* Do one step of sorting */
-
-  }
+  var timedWalkthrough = setInterval(selectionSortWalkthrough(list, i , j, k), 1000);
 }
 
 /* Kruskal's version of selection sort */
@@ -24,4 +23,32 @@ var selectionSort = function(list) {
     list[k] = tempVar;
   }
 
+}
+
+var selectionSortWalkthrough = function(list, i, j, k) {
+  /* This is one step within the sort function */
+  /* If i is greater than 0 then the sort isn't finished */
+  if (i > 0) {
+    /* Check that j doesn't need to be reset */
+    if (j < i) {
+      if (list[j] > list[k]) {
+        k = j;
+      }
+    } else {
+      /* This is the end of the for loop so update k and j appropriately */
+      if (j = i) {
+        if (list[j] > list[k]) {
+          k = j;
+        }
+
+        /* Make switches in the list if necessary and reset variables */
+        var tempVar = list[i];
+        list[i] = list[k];
+        list[k] = tempVar;
+
+        k = 0;
+      }
+    }
+
+  }
 }
