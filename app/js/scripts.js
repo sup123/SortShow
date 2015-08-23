7f1f7bc19143e4a549c1d190e51efa9d291d6b00
garbageman <damien.j.nagle@gmail.com>
2015-08-23 19:09:47 -0400

    I'm not sure what I changed

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -11,7 +11,7 @@ var sortStep = function(list, timer) {
 /* Kruskal's version of selection sort */
 var selectionSort = function(list) {
 
-  for (var i = list.length - 1; i >= 1; i--) {
+  for (var i = list.length - 1; i > 0; i--) {
     var k = 0;
     for (var j = 1; j <= i; j++) {
       if (list[j] > list[k]) {
@@ -23,4 +23,5 @@ var selectionSort = function(list) {
     list[i] = list[k];
     list[k] = tempVar;
   }
+
 }
